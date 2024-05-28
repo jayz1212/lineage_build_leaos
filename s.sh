a94db60825e4d6bcadf26fdc4eb4ec515737fcbf
jayz1212 <160797500+jayz1212@users.noreply.github.com>
2024-05-28 19:14:26 +0800

    f

---
+++ s.sh
@@ -0,0 +1,9 @@
+sign_target_files_apks -o -d ./android-certs \
+    out/target/product/phhgsi_arm64_ab/system/img \
+    signed-target_files.img
+
+
+ota_from_target_files -k ./android-certs/releasekey \
+    --block --backup=true \
+    signed-target_files.img \
+    signed-ota_update.img
