150f4ea4d60de45070c3a9e549d1a989856d2d4c
jayz1212 <160797500+jayz1212@users.noreply.github.com>
2024-05-28 13:23:14 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -214,7 +214,8 @@ echo "PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/extra/keys/releasekey" > vendor/
 fi
 
 
-
+echo "-include vendor/extra/product.mk" >> device/phh/treble/treble_arm_bvZ.mk
+cat device/phh/treble/treble_arm_bvZ.mk
 
 
 
