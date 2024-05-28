1c9bf0e7b4ecb8b5823a0df91972888903955181
jayz1212 <160797500+jayz1212@users.noreply.github.com>
2024-05-28 19:43:17 +0800

    d

--- s.sh
+++ s.sh
@@ -1,12 +1,17 @@
 source build/envsetup.sh
 
+MAIN=/tmp/src/android
+OUT=$MAIN/out/target/product/phhgsi_arm64_ab/system.img
 
-sign_target_files_apks -o -d ./android-certs \
-    out/target/product/phhgsi_arm64_ab/system/img \
-    signed-target_files.img
+cp -r $MAIN/.android-certs $HOME/.android-certs
 
 
-ota_from_target_files -k ./android-certs/releasekey \
+cd out/target/product/phhgsi_arm64_ab
+sign_target_files_apks -o -d ~/.android-certs \
+system.img \
+sign.img
+
+ota_from_target_files -k ~/.android-certs/releasekey \
     --block --backup=true \
-    signed-target_files.img \
-    signed-ota_update.img
+    sign.img \
+    signed.img
