0a55032170c1f21c9159f9f516826ca20b9981fa
jayz1212 <160797500+jayz1212@users.noreply.github.com>
2024-05-29 06:06:30 +0800

    Update s.sh

--- s.sh
+++ s.sh
@@ -1,18 +1,3 @@
 source build/envsetup.sh
-
-MAIN=/tmp/src/android
-OUT=$MAIN/out/target/product/phhgsi_arm64_ab
-
-cp -r $MAIN/android-certs $HOME/.android-certs
-
-
-croot
-./build/tools/releasetools/sign_target_files_apks -o -d ~/.android-certs \
-    $OUT/obj/PACKAGING/target_files_intermediates/*-target_files-*.zip \
-    signed-target_files.zip
-
-
-./build/tools/releasetools/ota_from_target_files -k ~/.android-certs/releasekey \
-    --block --backup=true \
-    signed-target_files.zip \
-    signed-ota_update.zip
+    breakfast treble_arm64_bvZ userdebug
+    brunch treble_arm64_bvZ
