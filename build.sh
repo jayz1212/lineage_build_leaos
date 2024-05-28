f295d789a1effdc9384fb58c7d4408f7a9b9b44b
jayz1212 <160797500+jayz1212@users.noreply.github.com>
2024-05-28 20:04:19 +0800

    s

--- build.sh
+++ build.sh
@@ -224,6 +224,20 @@ cat device/phh/treble/treble_arm_bvZ.mk
     lunch ${TARGET}-userdebug
     mka target-files-package otatools
 
+    cp -r $MAIN/android-certs $HOME/.android-certs
+
+
+croot
+./build/tools/releasetools/sign_target_files_apks -o -d ~/.android-certs \
+    $OUT/obj/PACKAGING/target_files_intermediates/*-target_files-*.zip \
+    signed-target_files.zip
+
+
+./build/tools/releasetools/ota_from_target_files -k ~/.android-certs/releasekey \
+    --block --backup=true \
+    signed-target_files.zip \
+    signed-ota_update.zip
+
 
 
 
--- s.sh
+++ s.sh
@@ -6,12 +6,13 @@ OUT=$MAIN/out/target/product/phhgsi_arm64_ab/system.img
 cp -r $MAIN/android-certs $HOME/.android-certs
 
 
-#cd out/target/product/phhgsi_arm64_ab
+croot
 ./build/tools/releasetools/sign_target_files_apks -o -d ~/.android-certs \
-out/target/product/phhgsi_arm64_ab/system.img \
-sign.img
+    $OUT/obj/PACKAGING/target_files_intermediates/*-target_files-*.zip \
+    signed-target_files.zip
 
-./build/tools/releasetools/sign_target_files_apks -k ~/.android-certs/releasekey \
+
+./build/tools/releasetools/ota_from_target_files -k ~/.android-certs/releasekey \
     --block --backup=true \
-    sign.img \
-    signed.img
+    signed-target_files.zip \
+    signed-ota_update.zip
