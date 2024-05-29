d6d6bf3fa36189e17f2a50f35e50939f811b9ec9
jayz1212 <160797500+jayz1212@users.noreply.github.com>
2024-05-29 12:04:31 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -224,7 +224,7 @@ cat device/phh/treble/treble_arm_bvZ.mk
 
 
    lunch ${TARGET}-userdebug
-   make clean
+ #  make clean
    make bacon
    make -j$(nproc --all) systemimage
    #mka target-files-package otatools
